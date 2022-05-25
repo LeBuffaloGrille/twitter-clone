8ccdc4c864ee3ba54e398e8a0097be6d0e1c81d3
LAPTOP-6JTA04B5\Ecole <matteo.bienaime@mewo-campus.fr>
2022-05-25 17:07:57 +0200

    Mise en place des commentaires.

---
+++ client/src/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from "prop-types"
+
+
+function Login(){
+
+    return(
+        <div></div>
+    );
+}
+
+Comment.propTypes = {comment: PropTypes.array};
+
+export default Comment;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}></Route>
+      <Route path="/login" element={<App/>}></Route>
+      <Route path="/user" element={<App/>}></Route>
+    </Routes>
+  </BrowserRouter>
 );
